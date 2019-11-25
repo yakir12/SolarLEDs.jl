3b5d053df4d3227939b379b03065f334e0d216f6
yakir12 <12.yakir@gmail.com>
2019-11-25 08:40:20 +0100

    corrected the max leds

--- src/SolarLEDs.jl
+++ src/SolarLEDs.jl
@@ -112,7 +112,7 @@ function main(; n_leds_per_strip::Int = 150, cardinals = [:EW, :NS], elevations 
     main(Setup(nsuns, n_leds_per_strip, cardinals, elevations, radii, intensities))
 end
 
-sun_wind_switch() = main(n_leds_per_strip = 141, elevations = [5, 20, 45, 60, 75, 80, 82, 84, 86, 88, 90], radii = [0], intensities = [255])
+sun_wind_switch() = main(n_leds_per_strip = 150, elevations = [5, 20, 45, 60, 75, 80, 82, 84, 86, 88, 90], radii = [0], intensities = [255])
 spaceship_dj() = main(n_leds_per_strip = 73, elevations = [1,2,3,4], radii = [0,1,2], intensities = 1:255, cardinals = [:NA])
 
 function goodport(port) 
